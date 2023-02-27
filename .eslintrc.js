d1ba77c29081664036236aa852697ff388806574
GatoRoy <roy_iphone@yahoo.com>
2023-02-28 00:10:34 +0200

    package.json + more settings

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'import/imports-first': 0,
+    'max-len': 0,
+    'newline-per-chained-call': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 1,
+    'no-use-before-define': 0,
+    'prefer-template': 2,
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        'prettier/prettier': ['warn', prettierOptions],
+      },
+    },
+  ],
+};
