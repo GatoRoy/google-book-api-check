a6ee0ff278d1b6d38a8c80fc38f0d3a9fbb468ae
GatoRoy <roy_iphone@yahoo.com>
2023-02-28 05:52:50 +0200

    styling LoginPage

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -3,6 +3,8 @@ import { useApp } from 'store/hooks/UseApp/Context';
 import { Box } from 'components/controls/Box';
 import { Typography } from 'components/controls/Typography';
 import { Button } from 'components/controls/buttons/Button';
+import { Textbox } from 'components/controls/Textbox';
+import { StyledLoginPageRow } from './styled';
 
 interface LoginPageProps {}
 
@@ -13,10 +15,30 @@ const LoginPage: FC<LoginPageProps> = props => {
     setIfLoggedIn(true);
   };
 
+  const handleNothing = () => {};
+
   return (
     <Box>
-      <Typography variant="h4">Login page</Typography>
-      <Button onClick={onLogin}>Click to login</Button>
+      <StyledLoginPageRow>
+        <Typography variant="h4">Login page</Typography>
+      </StyledLoginPageRow>
+      <StyledLoginPageRow>
+        <Textbox
+          className="username-input"
+          onChange={handleNothing}
+          placeholder="User name"
+        />
+      </StyledLoginPageRow>
+      <StyledLoginPageRow>
+        <Textbox
+          className="password-input"
+          onChange={handleNothing}
+          placeholder="Password"
+        />
+      </StyledLoginPageRow>
+      <StyledLoginPageRow>
+        <Button onClick={onLogin}>Login</Button>
+      </StyledLoginPageRow>
     </Box>
   );
 };
