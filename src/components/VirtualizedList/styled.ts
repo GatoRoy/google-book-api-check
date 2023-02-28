97696c62f2f38b49ca8334ce4c0892c5b9f4bb1a
GatoRoy <roy_iphone@yahoo.com>
2023-02-28 06:44:24 +0200

    removing redundant code

--- src/components/VirtualizedList/styled.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles';
-import { Box } from 'components/controls/Box';
-
-export const StyledVirtualizedList = styled(Box)`
-  width: '100%';
-  height: 400px;
-  max-width: 360px;
-  background-color: ${({ theme }) => theme.layout.paper.background};
-`;
